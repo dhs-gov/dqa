41145c9c94bd895a4e1ffec60f05a5eea6d74b20
StephenQuirolgico <stephen.quirolgico@gmail.com>
2021-11-08 15:14:21 -0500

    Removed file extension.

--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
 # DQA
+[![Python 3.8](https://img.shields.io/badge/python-3.8-blue.svg)](https://www.python.org/downloads/release/python-380/) [![Release](https://img.shields.io/badge/release-v0.1-blue)](https://img.shields.io/badge/release-v0.1-blue)
+
+## Installation
+To install with pypi, use:
+```
+pip install dqa
+```
+
+## Quick Start
+Open file ```example.py``` and change ```file_path``` to point to your 
+(context) document. Note that this version currently only supports ```.docx``` files. 
+
+If you want to use predefined questions, set ```questions``` to list of questions:
+```
+questions = ['What is the subject?', 'Who bought the food?']
+```
+If you want to pose questions interactively, set ```questions``` to None:
+```
+questions = None
+```
+To run ```example.py```, use:
+```
+python example.py
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='dqa',  
+     version='0.1',
+     scripts=['dqa_pip'] ,
+     author="Stephen Quirolgico",
+     author_email="stephen.quirolgico@gmail.com",
+     description="QA system",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/dhs-gov/dqa",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Public Domain",
+         "Operating System :: OS Independent",
+         "Intended Audience :: Science/Research",
+         "Intended Audience :: Developers",
+         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+     ],
+ )
